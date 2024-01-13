11dbc22f1afc87d3676bcf9477bbf0a4e9433c33
Anmol raghav <araghav181@gmail.com>
2024-01-13 14:22:22 +0530

    utils created

---
+++ src/utils/apiError.js
@@ -0,0 +1,18 @@
+class apiError extends Error {
+    constructor(
+        statusCode,
+        message : "Something went wrong",
+        errors : [],
+        stack : ""
+
+    ) {
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.errors = errors
+        this.success = false;
+
+    }
+}
+export {apiError}
