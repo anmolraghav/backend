4f15775d631016a60ed8e63f6e12af0b7123e5c5
Anmol raghav <araghav181@gmail.com>
2024-01-23 17:53:29 +0530

    update app.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,9 +7,9 @@ import {registerUser,loginUser, logoutUser,refreshAccessToken,
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router  = Router();
+const UserRouter = Router();
 
-router.route("/register").post(
+UserRouter.route("/register").post(
     upload.fields([
         {
             name:"avatar",
@@ -23,18 +23,18 @@ router.route("/register").post(
 registerUser
 )
 
-router.route("/login").post(loginUser)
+UserRouter.route("/login").post(loginUser)
 
 //secured routes
-router.route("/logout").post(verifyJWT,logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
-router.route("/change-password").post(verifyJWT,changeCurrentPassword)
-router.route("/current-user").get(verifyJWT,getCurrentUser)
-router.route("/update-account").patch(verifyJWT,updateAccountDetails)
-router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUsercoverImage)
-router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
-router.route("/history").get(verifyJWT,getWatchHistory)
+UserRouter.route("/logout").post(verifyJWT,logoutUser)
+UserRouter.route("/refresh-token").post(refreshAccessToken)
+UserRouter.route("/change-password").post(verifyJWT,changeCurrentPassword)
+UserRouter.route("/current-user").get(verifyJWT,getCurrentUser)
+UserRouter.route("/update-account").patch(verifyJWT,updateAccountDetails)
+UserRouter.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+UserRouter.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUsercoverImage)
+UserRouter.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+UserRouter.route("/history").get(verifyJWT,getWatchHistory)
 
  
-export default router
+export default UserRouter
